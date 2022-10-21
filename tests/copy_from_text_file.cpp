75aa19a9beafaba04ded2ef63bd57430f1271d5b
Hornyák Dániel <39958504+danielhornyak@users.noreply.github.com>
2022-10-21 18:01:11 +0200

    t

--- tests/copy_from_text_file.cpp
+++ tests/copy_from_text_file.cpp
@@ -15,9 +15,10 @@ string CopyTextFromAFile::copy_given_chapter(int wichChapter)
     ifstream MyReadFile("story.txt");
     if (MyReadFile.is_open())
     {
-        while (MyReadFile)
+        while (!MyReadFile.eof())
         {
-            myChar = MyReadFile.get();
+            //myChar = MyReadFile.get();
+            getline(MyReadFile, myChar);
             myText += myChar;
         }
     }
@@ -25,19 +26,19 @@ string CopyTextFromAFile::copy_given_chapter(int wichChapter)
     size_t chapterStartCharacterNumeber = myText.find(wichChapterSrting) + 1;
     if (chapterStartCharacterNumeber != 0)
     {
-        int wichChapterSrtingLenght = wichChapterSrting.length() + 1;
+        int wichChapterSrtingLenght = wichChapterSrting.length() ;
         size_t line;
         if (myText.find(SEPARATOR + to_string(wichChapter + 1)) != -1)
         {
             line = myText.find(SEPARATOR + to_string(wichChapter + 1));
-            copylenght = line - (chapterStartCharacterNumeber + wichChapterSrtingLenght+2);
+            copylenght = line - (chapterStartCharacterNumeber + wichChapterSrtingLenght);
         }
         else
         {
             line = myText.length();
-            copylenght = line - (chapterStartCharacterNumeber + wichChapterSrtingLenght + 1);
+            copylenght = line - (chapterStartCharacterNumeber + wichChapterSrtingLenght);
         }
-        myText.copy(copiedtext, copylenght, (chapterStartCharacterNumeber + wichChapterSrtingLenght));
+        myText.copy(copiedtext, copylenght+1, (chapterStartCharacterNumeber + wichChapterSrtingLenght-1));
         stringCopiedText = copiedtext;
     }
     else
--- tests/copytextfromtxtfile.cc
+++ tests/copytextfromtxtfile.cc
@@ -1,12 +1,12 @@
 #include "copy_from_text_file.cpp"
 #include <gtest/gtest.h>
 
-TEST(copytextfromtxt, Handelsallinputs) {
+TEST(copytextfromtxtc, Handelsallinputs) {
   CopyTextFromAFile object1;
-  EXPECT_EQ(object1.copy_given_chapter(0),"nincs ilyen chapter");
-  EXPECT_EQ(object1.copy_given_chapter(1),"sdtfgzudszgafgzesd");
-  EXPECT_EQ(object1.copy_given_chapter(2),"sdatzfzufsda");
-  EXPECT_EQ(object1.copy_given_chapter(3),"fzuestgauihsaedfzu");
+  ASSERT_EQ(object1.copy_given_chapter(0),"nincs ilyen chapter");
+  ASSERT_EQ(object1.copy_given_chapter(1),"\r\rsdtfgzudszgafgzesd\r\r");
+  ASSERT_EQ(object1.copy_given_chapter(2),"\r\rsdatzfzufsda\r\r");
+  ASSERT_EQ(object1.copy_given_chapter(3),"\r\rfzuestgauihsaedfzu");
 }
 
 
